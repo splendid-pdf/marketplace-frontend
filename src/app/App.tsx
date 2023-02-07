1f3383a66bec95bb91095ebc385ee63a0759c6b0
mlatysheva <mlatysheva@gmail.com>
2023-02-07 09:47:46 +0100

    fix: correct import of Footer and NavBar to pass github build check

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,9 +8,9 @@ import { HomePage } from '../pages/HomePage/HomePage';
 import { UserProfilePage } from '../pages/UserProfilePage/UserProfilePage';
 import { ContactsPage } from '../pages/ContactsPage/ContactsPage';
 import { NotFoundPage } from '../pages/NotFoundPage/NotFoundPage';
-import { Footer } from '../widgets/Footer';
-import { NavBar } from '../widgets/NavBar';
 import { Header } from '../widgets/Header/ui/Header';
+import { NavBar } from '../widgets/NavBar/ui/NavBar';
+import { Footer } from '../widgets/Footer/ui/Footer';
 
 const App = () => {
   const baseUrl = BASE_URL;
