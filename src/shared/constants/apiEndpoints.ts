30deb9a21b81428e24e6853cde87793b3afaea86
mlatysheva <mlatysheva@gmail.com>
2023-02-13 22:18:08 +0100

    chores: install redux-store, set up empty store and hooks

---
+++ src/shared/constants/apiEndpoints.ts
@@ -0,0 +1,6 @@
+// These endpoints do not exist yet
+// TODO: change to actual endpoints when the server is ready
+export const registerBuyer = 'http://51.250.102.12:8080/public/api/v1/users/registerBuyer/';
+export const loginBuyer = 'http://51.250.102.12:8080/public/api/v1/users/loginBuyer/';
+export const registerSeller = 'http://51.250.102.12:8080/public/api/v1/users/registerSeller/';
+export const loginSeller = 'http://51.250.102.12:8080/public/api/v1/users/loginSeller/';
