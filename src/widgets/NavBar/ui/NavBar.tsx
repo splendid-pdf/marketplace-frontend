56b26281f2c71594c444d902c4e545137a36d39e
mlatysheva <mlatysheva@gmail.com>
2023-02-17 21:30:53 +0100

    fix: change icons

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -5,10 +5,10 @@ import { BASE_URL } from "../../../shared/constants/base_url";
 import { setItemToLS } from "../../../shared/utils/setItemToLS";
 import { LS_KEY_ROLE } from "../../../shared/constants/localStorage";
 import { IconWithText } from "shared/ui/IconWithText/IconWithText";
-import HomeRepairServiceOutlinedIcon from '@mui/icons-material/HomeRepairServiceOutlined';
-import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import { ReactComponent as DeliveryIcon} from "../../../shared/images/icons/delivery.svg";
+import { ReactComponent as PersonIcon } from "../../../shared/images/icons/person.svg";
+import { ReactComponent as FavoriteIcon } from "../../../shared/images/icons/favorite.svg";
+import { ReactComponent as CartIcon } from "../../../shared/images/icons/cart.svg";
 
 export const NavBar = () => {
   const baseUrl = BASE_URL;
@@ -17,16 +17,16 @@ export const NavBar = () => {
     <div className={classes.nav}>
       <div className={`${classes.wrapperNavBar} container`}>
         <Link to={`/${baseUrl}/delivery`}>
-          <IconWithText icon={<HomeRepairServiceOutlinedIcon />} text={'Доставка'} />
+          <IconWithText icon={<DeliveryIcon />} text={'Доставка'} />
         </Link>
         <Link to={`?popup=login`} onClick={() => setItemToLS(LS_KEY_ROLE, 'buyer')}>
-          <IconWithText icon={<PermIdentityOutlinedIcon />} text={'Войти'} />
+          <IconWithText icon={<PersonIcon />} text={'Войти'} />
         </Link>
         <Link to={`/${baseUrl}/favorites`}>
-          <IconWithText icon={<FavoriteBorderOutlinedIcon />} text={'Избранное'} />
+          <IconWithText icon={<FavoriteIcon />} text={'Избранное'} />
         </Link>
         <Link to={`/${baseUrl}/cart`}>
-          <IconWithText icon={<ShoppingBagOutlinedIcon />} text={'Корзина'} />
+          <IconWithText icon={<CartIcon />} text={'Корзина'} />
         </Link>
       </div>
     </div>
