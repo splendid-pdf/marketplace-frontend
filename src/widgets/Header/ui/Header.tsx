d364214d265bff110c732773e26e7fdae2aa12c8
nikitanevskij <bike.rental@yandex.by>
2023-02-17 16:47:26 +0300

    feat: add container class

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -6,9 +6,13 @@ import SearchBar from '../searchBar/searchBar';
 export const Header: React.FC = () => {
   return (
     <div className={classes.Header}>
-      <h2 style={{ fontSize: 24, marginBottom: 10 }}>Header</h2>
-      <UserLocation />
-      <SearchBar />
+      <div className={classes.container}>
+        <div className={classes.headerWrapper}>
+          <h2 style={{ fontSize: 24, marginBottom: 10 }}>Header</h2>
+          <UserLocation />
+          <SearchBar />
+        </div>
+      </div>
     </div>
   );
 };
