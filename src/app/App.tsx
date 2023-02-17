fc20bc4498e357f849d54ab0a2213efbc5d339d3
mlatysheva <mlatysheva@gmail.com>
2023-02-17 22:03:47 +0100

    feat: add MUI theme to app component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,27 +4,47 @@ import { BrowserRouter } from 'react-router-dom';
 import Routing from 'pages';
 import ErrorBoundary from './ErrorBoundary';
 import { Spinner } from '../shared/ui/Spinner/Spinner';
-
 import { NavBar } from '../widgets/NavBar';
 import { Header } from '../widgets/Header';
 import { Footer } from '../widgets/Footer';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { ThemeProvider } from '@mui/material';
+import { grey} from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F15513'
+    },
+    secondary: grey //  TODO: Поставить второй цвет от дизайнеров и Разобраться, как его можно использовать
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold:600,
+  }
+});
 
 const App = () => {
   return (
     <div className={classes.App}>
       <ErrorBoundary>
-        <BrowserRouter>
-          <Suspense fallback={<Spinner />}>
-            <Provider store={store}>
-              <Header />
-              <NavBar />
-              <Routing />
-              <Footer />
-            </Provider>
-          </Suspense>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Suspense fallback={<Spinner />}>
+              <Provider store={store}>
+                <Header />
+                <NavBar />
+                <Routing />
+                <Footer />
+              </Provider>
+            </Suspense>
+          </BrowserRouter>
+        </ThemeProvider>
       </ErrorBoundary>
     </div>
   );
