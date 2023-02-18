b02751ece40dbb417d3f6bace01ce663b280320a
Stanislav Prokofjev <Prokostas@gmail.com>
2023-02-18 18:49:10 +0200

    added public and seller footer with layout

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { BASE_URL } from 'shared/constants/base_url';
+import PublicFooter from './publicFooter/PublicFooter';
 import classes from './Footer.module.scss';
-import { Link as LinkMUI } from '@mui/material';
-import { setItemToLS } from '../../../shared/utils/setItemToLS';
-import { LS_KEY_ROLE } from '../../../shared/constants/localStorage';
+import BottomFooter from './bottomFooter/BottomFooter';
+import SellerFooter from './sellerFooter/SellerFooter';
 
-// LinkMUI временное решение, кто будет делать footer, можно удалять и ссылку вешать на свой элемент.
-// LinkMUI, это переименованная Link от material, т.к. получается конфликт с Linkой от react-router-dom.
 
 export const Footer: React.FC = () => {
+
   return (
     <div className={classes.Footer}>
-      <div className={classes.wrapperFooter + ' container'}>
-        <h2 style={{ fontSize: 24 }}>Footer</h2>
-        <LinkMUI
-          to={`/${BASE_URL}/registerSeller`}
-          underline="none"
-          component={Link}
-          onClick={() => setItemToLS(LS_KEY_ROLE, 'seller')}
-        >
-          Создать аккаунт продавца
-        </LinkMUI>
-      </div>
+      <PublicFooter/>
+      {/* <SellerFooter/> */}
+      <BottomFooter/>
     </div>
+      
   );
 };
+
+
---
+++ src/widgets/Footer/ui/bottomFooter/BottomFooter.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './BottomFooter.module.scss';
+
+export const BottomFooter: React.FC = () => {
+  return (
+    <div className={classes.Footer}>
+      <div className={classes.FooterContainer}>
+        <div className={classes.FooterAgreement}>
+          <p className={classes.AgreementText}>Соглашение о конфиденциальности</p>
+          <p className={classes.AgreementText}>Пользовательское Соглашение</p>
+        </div>
+        <div className={classes.FooterRights}>
+          <p className={classes.RightsText}>© 2023 ООО «ClickAndBuy». Все права защищены</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BottomFooter;
