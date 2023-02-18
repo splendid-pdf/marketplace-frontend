cc52a4132c9716b0b24b37d230e78f407242eb87
nikitanevskij <bike.rental@yandex.by>
2023-02-18 14:10:04 +0300

    fix: style-changes-register/auth-form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,16 @@
-import { Suspense } from 'react';
-import classes from './App.module.scss';
-import { BrowserRouter } from 'react-router-dom';
-import Routing from 'pages';
-import ErrorBoundary from './ErrorBoundary';
-import { Spinner } from '../shared/ui/Spinner/Spinner';
-import { NavBar } from '../widgets/NavBar';
-import { Header } from '../widgets/Header';
-import { Footer } from '../widgets/Footer';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import { ThemeProvider } from '@mui/material';
-import { grey} from '@mui/material/colors';
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#F15513'
-    },
-    secondary: grey //  TODO: Поставить второй цвет от дизайнеров и Разобраться, как его можно использовать
-  },
-  typography: {
-    fontFamily: 'Montserrat',
-    fontWeightLight: 300,
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
-    fontWeightBold:600,
-  }
-});
+import { Suspense } from "react";
+import classes from "./App.module.scss";
+import { BrowserRouter } from "react-router-dom";
+import Routing from "pages";
+import ErrorBoundary from "./ErrorBoundary";
+import { Spinner } from "../shared/ui/Spinner/Spinner";
+import { NavBar } from "../widgets/NavBar";
+import { Header } from "../widgets/Header";
+import { Footer } from "../widgets/Footer";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./styles/theme";
 
 const App = () => {
   return (
