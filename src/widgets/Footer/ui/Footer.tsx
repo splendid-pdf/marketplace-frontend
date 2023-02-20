8eb75d51d94d51c2d68aaf3f0e6111f1a2de231f
nikitanevskij <bike.rental@yandex.by>
2023-02-20 12:15:11 +0300

    fix: fix lint

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import PublicFooter from './publicFooter/PublicFooter';
-import classes from './Footer.module.scss';
-import BottomFooter from './bottomFooter/BottomFooter';
-import SellerFooter from './sellerFooter/SellerFooter';
-
+import React from "react";
+import PublicFooter from "./publicFooter/PublicFooter";
+import classes from "./Footer.module.scss";
+import BottomFooter from "./bottomFooter/BottomFooter";
+// import SellerFooter from './sellerFooter/SellerFooter';
 
 export const Footer: React.FC = () => {
-
   return (
     <div className={classes.Footer}>
-      <PublicFooter/>
+      <PublicFooter />
       {/* <SellerFooter/> */}
-      <BottomFooter/>
+      <BottomFooter />
     </div>
-      
   );
 };
-
-
