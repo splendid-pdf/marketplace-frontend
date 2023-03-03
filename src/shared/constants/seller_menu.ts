e680f0e84315465d34b000fc6f594c05304f2fdc
DianaZd <dipetrovnadi@gmail.com>
2023-03-03 22:09:30 +0100

    add menuSeller, SellerBaseOrdersPage,SellerBaseProductPage

---
+++ src/shared/constants/seller_menu.ts
@@ -0,0 +1,77 @@
+import { BASE_URL } from "shared/constants/base_url";
+
+export type TItems = {
+        name: string;
+        url: string;
+        links?: string[];
+        listMenu?: Array<{ name: string; url?: string }>;
+      };
+      
+
+const ProductsLinks = [
+  `${BASE_URL}/home-seller/product`,
+  `${BASE_URL}/home-seller/product/archive`,
+  `${BASE_URL}/home-seller/product/reviews`,
+  `${BASE_URL}/home-seller/product/questions`,
+  `${BASE_URL}/home-seller/product/recommendations`
+]
+
+const OrdersLinks =[
+  `${BASE_URL}/home-seller/orders`,
+  `${BASE_URL}/home-seller/orders/activeorders`,
+  `${BASE_URL}/home-seller/orders/completedorders`
+]
+
+export const menuItemsSeller: TItems[] = [
+  {
+    name: "Главная",
+    url: `${BASE_URL}/home-seller`,
+    listMenu: [],
+  },
+  {
+    name: "Товары",
+    url: `${BASE_URL}/home-seller/product`,
+    links:  ProductsLinks,
+    listMenu: [
+      {
+        name: "Список товаров",
+        url: `${BASE_URL}/home-seller/product`,
+      },
+      {
+        name: "Архив",
+        url: `${BASE_URL}/home-seller/product/archive`,
+      },
+      {
+        name: "Отзывы",
+        url: `${BASE_URL}/home-seller/product/reviews`,
+      },
+      {
+        name: "Вопросы",
+        url: `${BASE_URL}/home-seller/product/questions`,
+      },
+      {
+        name: "Рукомендации",
+        url: `${BASE_URL}/home-seller/product/recommendations`,
+      },
+    ],
+  },
+  {
+    name: "Заказы",
+    url: `${BASE_URL}/home-seller/orders`,
+    links:  OrdersLinks,
+    listMenu: [
+      {
+        name: "Все",
+        url: `${BASE_URL}/home-seller/orders`,
+      },
+      {
+        name: "Активные",
+        url: `${BASE_URL}/home-seller/orders/activeorders`,
+      },
+      {
+        name: "Завешенные",
+        url: `${BASE_URL}/home-seller/orders/completedorders`,
+      },
+    ],
+  },
+];
--- src/shared/ui/Button/ButtonMarketPlace.tsx
+++ src/shared/ui/Button/ButtonMarketPlace.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import classNames from "classnames";
 import { FC, ReactNode } from "react";
 import classes from "./ButtonMarketPlace.module.scss";
+import { Link} from "react-router-dom";
 
 export enum ButtonVariant {
   // eslint-disable-next-line no-unused-vars
@@ -10,32 +11,43 @@ export enum ButtonVariant {
   contained = "contained",
 }
 
+export enum BorderColorVarian {
+  // eslint-disable-next-line no-unused-vars
+  orange = "orange",
+  // eslint-disable-next-line no-unused-vars
+  grey = "grey",
+}
+
 interface IButtonMarketPlaceProps {
-  text: string;
-  icon?: ReactNode;
-  variant?: ButtonVariant;
-  className?: string;
-  onClick?: () => void;
+    text: string;
+    icon?: ReactNode;
+    variant?: ButtonVariant;
+    borderColorVarian?: BorderColorVarian;
+    className?: string;
+    to?:string
 }
 
 export const ButtonMarketPlace: FC<IButtonMarketPlaceProps> = ({
   text,
   icon,
-  onClick,
   variant = ButtonVariant.contained,
+  borderColorVarian = BorderColorVarian.orange,
   className,
+  to,
 }) => {
   return (
     <Button
       className={classNames(
         //  variant === ButtonVariant.outlined ? classes.outlined  : ''
-        variant === ButtonVariant.outlined && classes.outlined,
+        variant === ButtonVariant.outlined && classes.outlined, 
+        borderColorVarian === BorderColorVarian.grey && classes.greyBorder,
         className,
         classes.btn,
       )}
       variant={variant}
       endIcon={icon}
-      onClick={onClick}
+      component={Link}
+      to={`/${to}` || ''}
     >
       {text}
     </Button>
