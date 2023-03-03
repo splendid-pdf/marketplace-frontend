5ee991e55951cbe8ecd24a74a9f28bf830fce493
nikitanevskij <bike.rental@yandex.by>
2023-03-03 12:38:38 +0300

    feature: add modal actions in sellerProductPage

--- src/pages/SellerProductPage/content/productActions/ProductActions.tsx
+++ src/pages/SellerProductPage/content/productActions/ProductActions.tsx
@@ -1,18 +1,20 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
-import InputSearch from 'shared/ui/InputSearch/InputSearch';
-import classes from './ProductActions.module.scss';
+import InputSearch from "shared/ui/InputSearch/InputSearch";
+import classes from "./ProductActions.module.scss";
 import { ButtonMarketPlace } from "shared/ui/Button/ButtonMarketPlace";
-
+import { ModalDeleteProducts, ModalPutUpSale, ModalStopSale } from "../modalActions/modalActions";
 
 const ProductActions = () => {
-
   const [value, setValue] = useState("");
-
+  const [isOpenModalStopSale, setIsOpenModalStopSale] = useState(false);
+  const [isOpenModalPutUpSale, setIsOpenModalPutUpSale] = useState(false);
+  const [isOpenModalDeleteProducts, setIsOpenModalDeleteProducts] = useState(false);
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(value)  };
+    console.log(value);
+  };
 
   const onSearch = (searchTerm: string) => {
     console.log("search", searchTerm);
@@ -26,25 +28,36 @@ const ProductActions = () => {
         onSearch={onSearch}
         placeholder="Название товара или артикул"
       />
-      <ButtonMarketPlace
-        className={classes.btn}
-        text="Создать новый товар"
-      />
+      <ButtonMarketPlace className={classes.btn} text="Создать новый товар" />
       <ButtonMarketPlace
         className={classes.btn}
         text="Снять с продажи"
+        onClick={() => setIsOpenModalStopSale((prev) => !prev)}
+      />
+      <ModalStopSale
+        isOpened={isOpenModalStopSale}
+        onClose={() => setIsOpenModalStopSale((prev) => !prev)}
       />
       <ButtonMarketPlace
         className={classes.btn}
         text="Выставить на продажу"
+        onClick={() => setIsOpenModalPutUpSale((prev) => !prev)}
+      />
+      <ModalPutUpSale
+        isOpened={isOpenModalPutUpSale}
+        onClose={() => setIsOpenModalPutUpSale((prev) => !prev)}
       />
       <ButtonMarketPlace
         className={classes.btn}
         text="Удалить данные"
+        onClick={() => setIsOpenModalDeleteProducts((prev) => !prev)}
       />
-      
+      <ModalDeleteProducts
+        isOpened={isOpenModalDeleteProducts}
+        onClose={() => setIsOpenModalDeleteProducts((prev) => !prev)}
+      />
     </div>
   );
-}
+};
 
 export default ProductActions;
--- src/pages/SellerProductPage/content/table/ProductTable.tsx
+++ src/pages/SellerProductPage/content/table/ProductTable.tsx
@@ -1,57 +1,99 @@
-import { Product } from "shared/api/types"
-import React from 'react';
+import { Product } from "shared/api/types";
+import React from "react";
 import { ReactComponent as EditIcon } from "../../../../shared/images/icons/edit.svg";
 import { ReactComponent as DeleteIcon } from "../../../../shared/images/icons/delete.svg";
-import classes from './ProductTable.module.scss';
-import Checkbox from '@mui/material/Checkbox';
-
-
+import classes from "./ProductTable.module.scss";
+import Checkbox from "@mui/material/Checkbox";
+import { ModalDeleteOneProduct } from "../modalActions/modalActions";
 
 type Props = {
   products: Product[];
-}
+};
 
-export const ProductTable = ({products}: Props) => {
-
+export const ProductTable = ({ products }: Props) => {
+  const [isOpenModalDeleteOneProduct, setIsOpenModalDeleteOneProduct] = React.useState(false);
 
   return (
     <div className={classes.Table}>
+      <ModalDeleteOneProduct
+        isOpened={isOpenModalDeleteOneProduct}
+        onClose={() => setIsOpenModalDeleteOneProduct((prev) => !prev)}
+      />
       <div className={classes.TableHead}>
         <div className={classes.HeadRow}>
-          <Checkbox style={{padding: 0}}/>
-          <div className={classes.HeaderCell} style={{ width: '4rem' }}>Фото</div>
-          <div className={classes.HeaderCell} style={{ width: '10rem' }}>Название</div>
-          <div className={classes.HeaderCell} style={{ width: '5rem' }}>Артикул</div>
-          <div className={classes.HeaderCell} style={{ width: '5rem' }}>Цена</div>
-          <div className={classes.HeaderCell} style={{ width: '6rem' }}>Склад</div>
-          <div className={classes.HeaderCell} style={{ width: '6rem' }}>В продаже</div>
-          <div className={classes.HeaderCell} style={{ width: '6rem' }}>Категория</div>
-          <div className={classes.HeaderCell} style={{ width: '6rem' }}>Тип</div>
-          <div className={classes.HeaderCell} style={{ width: '8rem' }}>Дата создания</div>
-          <div className={classes.HeaderCell} style={{ width: '4rem' }}></div>
+          <Checkbox style={{ padding: 0 }} />
+          <div className={classes.HeaderCell} style={{ width: "4rem" }}>
+            Фото
+          </div>
+          <div className={classes.HeaderCell} style={{ width: "10rem" }}>
+            Название
+          </div>
+          <div className={classes.HeaderCell} style={{ width: "5rem" }}>
+            Артикул
+          </div>
+          <div className={classes.HeaderCell} style={{ width: "5rem" }}>
+            Цена
+          </div>
+          <div className={classes.HeaderCell} style={{ width: "6rem" }}>
+            Склад
+          </div>
+          <div className={classes.HeaderCell} style={{ width: "6rem" }}>
+            В продаже
+          </div>
+          <div className={classes.HeaderCell} style={{ width: "6rem" }}>
+            Категория
+          </div>
+          <div className={classes.HeaderCell} style={{ width: "6rem" }}>
+            Тип
+          </div>
+          <div className={classes.HeaderCell} style={{ width: "8rem" }}>
+            Дата создания
+          </div>
+          <div className={classes.HeaderCell} style={{ width: "4rem" }}></div>
         </div>
       </div>
       <div className={classes.TableBody}>
         {products.map((product) => (
           <div key={product.id} className={classes.BodyRow}>
-            <Checkbox style={{padding: 0}}/>
-            <div style={{ width: '4rem' }}>
-              <img className={classes.ContentRowImage} src={product.img} alt={product.title}/></div>
-            <div  className={classes.BodyCell} style={{ width: '10rem' }}>{product.title}</div>
-            <div  className={classes.BodyCell} style={{ width: '5rem' }}>{product.id}</div>
-            <div  className={classes.BodyCell} style={{ width: '5rem' }}>{product.price}</div>
-            <div  className={classes.BodyCell} style={{ width: '6rem' }}>{product.stock}</div>
-            <div  className={classes.BodyCell} style={{ width: '6rem' }}>
-              {product.sale ? 'есть' : 'нет'}
+            <Checkbox style={{ padding: 0 }} />
+            <div style={{ width: "4rem" }}>
+              <img className={classes.ContentRowImage} src={product.img} alt={product.title} />
             </div>
-            <div  className={classes.BodyCell} style={{ width: '6rem' }}>
+            <div className={classes.BodyCell} style={{ width: "10rem" }}>
+              {product.title}
+            </div>
+            <div className={classes.BodyCell} style={{ width: "5rem" }}>
+              {product.id}
+            </div>
+            <div className={classes.BodyCell} style={{ width: "5rem" }}>
+              {product.price}
+            </div>
+            <div className={classes.BodyCell} style={{ width: "6rem" }}>
+              {product.stock}
+            </div>
+            <div className={classes.BodyCell} style={{ width: "6rem" }}>
+              {product.sale ? "есть" : "нет"}
+            </div>
+            <div className={classes.BodyCell} style={{ width: "6rem" }}>
               {product.category.name}
             </div>
-            <div  className={classes.BodyCell} style={{ width: '6rem' }}>{product.type.name}</div>
-            <div  className={classes.BodyCell} style={{ width: '7rem' }}>{product.dateTime}</div>
-            <div className={classes.ButtonsCell} style={{ width: '5rem' }}>
-              <button className={classes.Button}><DeleteIcon/></button>
-              <button className={classes.Button}><EditIcon/></button>
+            <div className={classes.BodyCell} style={{ width: "6rem" }}>
+              {product.type.name}
+            </div>
+            <div className={classes.BodyCell} style={{ width: "7rem" }}>
+              {product.dateTime}
+            </div>
+            <div className={classes.ButtonsCell} style={{ width: "5rem" }}>
+              <button
+                className={classes.Button}
+                onClick={() => setIsOpenModalDeleteOneProduct((prev) => !prev)}
+              >
+                <DeleteIcon />
+              </button>
+
+              <button className={classes.Button}>
+                <EditIcon />
+              </button>
             </div>
           </div>
         ))}
@@ -59,6 +101,5 @@ export const ProductTable = ({products}: Props) => {
     </div>
   );
 };
-
 
 export default ProductTable;
