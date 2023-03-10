1676148718e7c3adfbf2e51a85f6d4a4561aee3d
Artur <bakhvalovwork@gmail.com>
2023-03-10 20:30:27 +0500

    fix

--- src/pages/SellerProductPage/SellerProductPage.tsx
+++ src/pages/SellerProductPage/SellerProductPage.tsx
@@ -3,10 +3,8 @@ import Content from "./content/Content";
 
 const SellerProductPage: React.FC = () => {
   return (
-    <div className={"content" + " " + "container"}>
-      <div>
-        <Content />
-      </div>
+    <div>
+      <Content />
     </div>
   );
 };
--- src/widgets/Promo/ui/Promo.tsx
+++ src/widgets/Promo/ui/Promo.tsx
@@ -1,52 +1,58 @@
 import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
 import "./style.css";
-
-// import required modules
 import { Pagination, Navigation } from "swiper";
+
+import promo1 from "shared/images/promo1.png";
+import promo2 from "shared/images/promo2.png";
+import promo3 from "shared/images/promo3.png";
 
 export const Promo = () => {
   return (
     <div className={"content" + " " + "container"}>
-      <>
+      <div className="container-promo">
+        <div className="titles">
+          <h1>Акции</h1>
+        </div>
         <Swiper
+          className="promo-swiper"
           slidesPerView={3}
-          spaceBetween={20}
-          loop={true}
+          speed={1500}
           pagination={{
             clickable: true,
+            type: "fraction",
           }}
-          navigation={true}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
           modules={[Pagination, Navigation]}
-          className="mySwiperq"
         >
-          <SwiperSlide>
-            <div className="q"></div>
+          <SwiperSlide className="qqq">
+            <img className="images" src={promo3} />
           </SwiperSlide>
-          <SwiperSlide>
-            <div className="q"></div>
+          <SwiperSlide className="qqq">
+            <img className="images" src={promo2} />
           </SwiperSlide>
-          <SwiperSlide>
-            <div className="q"></div>
-          </SwiperSlide>{" "}
-          <SwiperSlide>
-            <div className="q"></div>
+          <SwiperSlide className="qqq">
+            <img className="images" src={promo1} />
           </SwiperSlide>
-          <SwiperSlide>
-            <div className="q"></div>
+          <SwiperSlide className="qqq">
+            <img className="images" src={promo3} />
           </SwiperSlide>
-          <SwiperSlide>
-            <div className="q"></div>
+          <SwiperSlide className="qqq">
+            <img className="images" src={promo2} />
           </SwiperSlide>
+          <div className="slider-controler">
+            <div className="swiper-button-prev"></div>
+            <div className="swiper-button-next"></div>
+            <div className="swiper-pagination"></div>
+          </div>
         </Swiper>
-      </>
+      </div>
     </div>
   );
 };
