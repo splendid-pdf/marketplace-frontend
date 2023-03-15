358224f25dda0401101b10db8ff82d9d64a07e34
mlatysheva <mlatysheva@gmail.com>
2023-03-15 13:32:55 +0100

    fix: work on cors error

--- src/features/buyerAuth/model/slice/registerBuyer.ts
+++ src/features/buyerAuth/model/slice/registerBuyer.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { API_REGISTER_BUYER_URL } from 'shared/api/apiEndpoints';
 import { axiosInstance } from 'shared/api/axiosInstance';
 import { buyerAuthActions } from './buyerAuthSlice';
@@ -8,16 +8,23 @@ import { BuyerAuth } from '../types/BuyerAuthSchema';
 export const registerBuyer = createAsyncThunk(
   'buyerAuth/register',
   async (registerData: BuyerAuth, { rejectWithValue, dispatch }) => {
-    dispatch(buyerAuthActions.setRegData({id:'fake_id', role: 'buyer'}));
+    // dispatch(buyerAuthActions.setRegData({id:'fake_id', role: 'buyer'}));
+    const headers = {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin" : API_REGISTER_BUYER_URL, // your request url
+      "Access-Control-Allow-Methods" : "POST, GET, OPTIONS" // supported methods
+    }
     try {
-      const res: AxiosResponse = await axiosInstance.post(
-        API_REGISTER_BUYER_URL, 
-        JSON.stringify({ registerData}),
-        {
-          headers: {'Content-Type': 'application/json'},
-        }
+      // const res: AxiosResponse = await axiosInstance.post(
+      //   API_REGISTER_BUYER_URL, 
+      //   registerData,
+      // );
+      const res = await axios.post(
+        API_REGISTER_BUYER_URL,
+        registerData,
+        // { headers: headers }
       );
-      dispatch(buyerAuthActions.setRegData({id: res.data.id, role: res.data.role}));
+      dispatch(buyerAuthActions.setRegData({id: res.data.id}));
       return res.data;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
--- src/shared/api/apiEndpoints.ts
+++ src/shared/api/apiEndpoints.ts
@@ -1,8 +1,10 @@
 // These endpoints do not exist yet
 // TODO: change to actual endpoints when the server is ready
 
-export const API_BASE_URL = 'http://51.250.102.12:8080/';
-export const API_REGISTER_BUYER_URL = 'public/api/v1/users/signup';
-export const API_LOGIN_BUYER_URL = 'public/api/v1/users/login';
+export const API_BASE_URL = 'https://d5dgfhb917mq6267t8v5.apigw.yandexcloud.net/public/api/v1';
+export const API_REGISTER_BUYER_URL = `${API_BASE_URL}/users/signup`;
+export const API_LOGIN_BUYER_URL = `${API_BASE_URL}/oauth/login`;
+export const API_LOGOUT_BUYER_URL = `${API_BASE_URL}/oauth/logout`;
 export const API_REGISTER_SELLER_URL = 'registerSeller/';
 export const API_LOGIN_SELLER_URL = 'loginSeller/';
+
