33d8035f88062e133ca6bde3fe58e6306e18cc9a
nikitanevskij <bike.rental@yandex.by>
2023-03-17 11:24:09 +0300

    fix types products seller

--- src/shared/api/types.ts
+++ src/shared/api/types.ts
@@ -1,15 +1,20 @@
 export type Product = {
-  productImages: string;
-  externalId: number;
-  name: string;
-  price: number;
-  articleFromSeller: string;
-  sale: boolean;
-  count: number;
-  isVisible: true;
+  productImages?: string;
+  externalId?: number;
+  id?: number;
+  img?: string;
+  title?: string;
+  stock?: number;
+  dateTime?: string;
+  name?: string;
+  price?: number;
+  articleFromSeller?: string;
+  sale?: boolean;
+  count?: number;
+  isVisible?: true;
   category: Category;
   type: Type;
-  creationDate: string;
+  creationDate?: string;
 };
 
 export type Category = {
