e76e462aab39ee724da7358ef33acd5c592495f4
Artur <bakhvalovwork@gmail.com>
2023-04-03 16:11:11 +0500

    lk-seller

--- src/entities/SellerProfile/ui/SellerProfileCard/SellerProfileCard.tsx
+++ src/entities/SellerProfile/ui/SellerProfileCard/SellerProfileCard.tsx
@@ -33,206 +33,203 @@ export const SellerProfileCard = () => {
 
   return (
     <div className={`${classes.SellerProfileCard}`}>
-      <form className={classes.form}>
-        <div className={classes.ordersHeader__linkWrapper}>
-          <NavLink to={`/${BASE_URL}/seller/account`} className={setActive}>
-            ИП
-          </NavLink>
-          <NavLink to={`/${BASE_URL}/seller/account/OOO`} className={setActive}>
-            ООО
-          </NavLink>
-        </div>
-        <Typography
-          variant="h5"
-          sx={{
-            marginBottom: 5,
-            fontFamily: "Manrope, sans-serif",
-            fontSize: 36,
-          }}
-        >
-          Личные данные
-        </Typography>
-        <Avatar
-          sx={{
-            alignSelf: "left",
-            width: "160px",
-            height: "160px",
-          }}
-        />
-        <ButtonBase
-          sx={{
-            width: "160px",
-            alignSelf: "left",
-            textAlign: "left",
-            marginTop: "10px",
-            fontSize: "16px",
-          }}
-        >
-          Изменить фото
-        </ButtonBase>
-        <Typography
-          sx={{
-            fontFamily: "Manrope, sans-serif",
-            fontSize: 22,
-            marginTop: "30px",
-            marginBottom: "20px",
-          }}
-        >
-          Имя
-        </Typography>
-        <TextField className={classes.field} placeholder="Имя" size="small" />
-        <TextField
-          className={classes.field}
-          placeholder="Фамилия"
-          size="small"
-          disabled={readonly}
-        />
-        <Typography
-          sx={{
-            fontFamily: "Manrope, sans-serif",
-            fontSize: 22,
-            marginTop: "20px",
-            marginBottom: "30px",
-          }}
-        >
-          Адрес регистрации
-        </Typography>
-        <TextField
-          className={classes.field}
-          placeholder="Номер телефона"
-          size="small"
-          disabled={readonly}
-        />
-
-        <Typography
-          sx={{
-            className: "qwe",
-            fontFamily: "Manrope, sans-serif",
-            fontSize: 22,
-            marginTop: "20px",
-            marginBottom: "30px",
-          }}
-        >
+      <div className={classes.ordersHeader__linkWrapper}>
+        <NavLink to={`/${BASE_URL}/seller/account`} className={setActive}>
           ИП
-        </Typography>
-        <TextField
-          className={classes.field}
-          placeholder="Введите название ИП"
-          size="small"
-          disabled={readonly}
-        />
-        <Typography
-          sx={{
-            fontFamily: "Manrope, sans-serif",
-            fontSize: 22,
-            marginTop: "20px",
-            marginBottom: "30px",
-          }}
-        >
-          ИНН / ОГРНИП
-        </Typography>
-        <TextField
-          className={classes.field}
-          placeholder="Введите  12 цифр"
-          size="small"
-          disabled={readonly}
-        />
-        <TextField
-          className={classes.field}
-          placeholder="Введите  12 цифр"
-          size="small"
-          disabled={readonly}
-        />
-        <Typography
-          variant="h5"
-          sx={{
-            marginTop: "60px",
-            marginBottom: "40px",
-            fontFamily: "Manrope, sans-serif",
-            fontSize: 36,
-          }}
-        >
-          Банковские данные
-        </Typography>
-        <Typography
-          sx={{
-            fontFamily: "Manrope, sans-serif",
-            fontSize: 22,
-            marginBottom: "30px",
-            marginTop: "20px",
-          }}
-        >
-          Расчетный счет
-        </Typography>
-        <TextField
-          className={classes.field}
-          placeholder="Введите 12 цифр"
-          size="small"
-          disabled={readonly}
-        />
-        <Typography
-          sx={{
-            fontFamily: "Manrope, sans-serif",
-            fontSize: 22,
-            marginBottom: "30px",
-            marginTop: "20px",
-          }}
-        >
-          Корпоративный счет
-        </Typography>
-        <TextField
-          className={classes.field}
-          placeholder="Введите 12 цифр"
-          size="small"
-        />
-        <Typography
-          sx={{
-            fontFamily: "Manrope, sans-serif",
-            fontSize: 22,
-            marginBottom: "30px",
-            marginTop: "20px",
-          }}
-        >
-          БИК
-        </Typography>
-        <TextField
-          className={classes.field}
-          placeholder="Введите 12 цифр"
-          size="small"
-          disabled={readonly}
-        />
-        <div className={`${classes.SellerProfileButtons}`}>
-          {readonly ? (
+        </NavLink>
+        <NavLink to={`/${BASE_URL}/seller/account/OOO`} className={setActive}>
+          ООО
+        </NavLink>
+      </div>
+      <Typography
+        variant="h5"
+        sx={{
+          marginBottom: 5,
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 36,
+        }}
+      >
+        Личные данные
+      </Typography>
+      <Avatar
+        sx={{
+          alignSelf: "left",
+          width: "160px",
+          height: "160px",
+        }}
+      />
+      <ButtonBase
+        sx={{
+          width: "160px",
+          alignSelf: "left",
+          textAlign: "left",
+          marginTop: "10px",
+          fontSize: "16px",
+        }}
+      >
+        Изменить фото
+      </ButtonBase>
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginTop: "30px",
+          marginBottom: "30px",
+        }}
+      >
+        Имя
+      </Typography>
+      <TextField className={classes.field} placeholder="Введите имя" size="small" />
+      <TextField
+        className={classes.field}
+        placeholder="Введите фамилию"
+        size="small"
+        disabled={readonly}
+      />
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginTop: "20px",
+          marginBottom: "30px",
+        }}
+      >
+        Адрес регистрации
+      </Typography>
+      <TextField
+        className={classes.field}
+        placeholder="Введите номер телефона"
+        size="small"
+        disabled={readonly}
+      />
+
+      <Typography
+        sx={{
+          className: "qwe",
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginTop: "20px",
+          marginBottom: "30px",
+        }}
+      >
+        ИП
+      </Typography>
+      <TextField
+        className={classes.field}
+        placeholder="Введите название ИП"
+        size="small"
+        disabled={readonly}
+      />
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginTop: "20px",
+          marginBottom: "30px",
+        }}
+      >
+        ИНН / ОГРНИП
+      </Typography>
+      <TextField
+        className={classes.field}
+        placeholder="Введите  12 цифр"
+        size="small"
+        disabled={readonly}
+      />
+      <TextField
+        className={classes.field}
+        placeholder="Введите  12 цифр"
+        size="small"
+        disabled={readonly}
+      />
+      <Typography
+        variant="h5"
+        sx={{
+          marginTop: "60px",
+          marginBottom: "40px",
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 36,
+        }}
+      >
+        Банковские данные
+      </Typography>
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginBottom: "30px",
+        }}
+      >
+        Расчетный счет
+      </Typography>
+      <TextField
+        className={classes.field}
+        placeholder="Введите 12 цифр"
+        size="small"
+        disabled={readonly}
+      />
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginBottom: "30px",
+          marginTop: "20px",
+        }}
+      >
+        Корпоративный счет
+      </Typography>
+      <TextField
+        className={classes.field}
+        placeholder="Введите 12 цифр"
+        size="small"
+      />
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginBottom: "30px",
+          marginTop: "20px",
+        }}
+      >
+        БИК
+      </Typography>
+      <TextField
+        className={classes.field}
+        placeholder="Введите 12 цифр"
+        size="small"
+        disabled={readonly}
+      />
+      <div className={`${classes.SellerProfileButtons}`}>
+        {readonly ? (
+          <Button
+            className={classes.button}
+            size="large"
+            variant="contained"
+            onClick={onEdit}
+          >
+            Редактировать
+          </Button>
+        ) : (
+          <>
             <Button
               className={classes.button}
+              type="submit"
               size="large"
               variant="contained"
-              onClick={onEdit}
+              onClick={onSave}
             >
-              Редактировать
+              Сохранить
             </Button>
-          ) : (
-            <>
-              <Button
-                className={classes.button}
-                type="submit"
-                size="large"
-                variant="contained"
-                onClick={onSave}
-              >
-                Сохранить
-              </Button>
-              <Button
-                className={classes.button}
-                variant="outlined"
-                onClick={onCancelEdit}
-              >
-                Отменить
-              </Button>
-            </>
-          )}
-        </div>
-      </form>
+            <Button
+              className={classes.button}
+              variant="outlined"
+              onClick={onCancelEdit}
+            >
+              Удалить
+            </Button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/SellerAccountPage/SellerShopsPage/ui/SellerShopsPage.tsx
+++ src/pages/SellerAccountPage/SellerShopsPage/ui/SellerShopsPage.tsx
@@ -1,19 +1,165 @@
-import { Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  ButtonBase,
+  TextField,
+  Typography,
+} from "@mui/material";
 import classes from "./SellerShopsPage.module.scss";
+import { useAppDispatch } from "app/store/hooks";
+import { useCallback, useState } from "react";
+import { ReactComponent as Logoload } from "../../../../shared/images/icons/logoload.svg";
 
 const SellerShopsPage = () => {
+  const dispatch = useAppDispatch();
+  const [readonly, setReadonly] = useState(true);
+
+  const onEdit = () => {
+    setReadonly(false);
+  };
+
+  const onCancelEdit = () => {
+    setReadonly(true);
+  };
+
+  const onSave = useCallback(() => {
+    // dispatch(updateBuyerProfileData());
+    setReadonly(true);
+  }, [dispatch]);
   return (
     <div className={`${classes.SellerShopsPage} rightWrapper`}>
       <Typography
         variant="h5"
         sx={{
-          marginBottom: 5,
+          marginBottom: "40px",
           fontFamily: "Manrope, sans-serif",
           fontSize: 36,
         }}
       >
         Список магазинов
       </Typography>
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginBottom: "30px",
+        }}
+      >
+        Название
+      </Typography>
+      <TextField
+        className={classes.field}
+        size="small"
+        placeholder="Введите название"
+      />
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginTop: "20px",
+          marginBottom: "30px",
+        }}
+      >
+        Категория / Тип
+      </Typography>
+      <TextField
+        className={classes.field}
+        size="small"
+        placeholder="Введите категорию"
+      />
+      <TextField
+        className={classes.field}
+        size="small"
+        placeholder="Введите тип"
+      />
+      <Typography
+        variant="h5"
+        sx={{
+          marginTop: "60px",
+          marginBottom: "40px",
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 36,
+        }}
+      >
+        Создать новый магазин
+      </Typography>
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginBottom: "30px",
+        }}
+      >
+        Название
+      </Typography>
+      <TextField
+        className={classes.field}
+        size="small"
+        placeholder="Введите название"
+      />
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginTop: "20px",
+          marginBottom: "30px",
+        }}
+      >
+        Адрес склада
+      </Typography>
+      <TextField
+        className={classes.field}
+        size="small"
+        placeholder="Введите адрес"
+      />
+      <Typography
+        sx={{
+          fontFamily: "Manrope, sans-serif",
+          fontSize: 22,
+          marginTop: "20px",
+          marginBottom: "30px",
+        }}
+      >
+        Лого
+      </Typography>
+      <Button
+        className={classes.button_small}
+        variant="outlined"
+        onClick={onCancelEdit}
+      >
+        <Logoload />
+      </Button>
+      <div className={`${classes.SellerProfileButtons}`}>
+        {readonly ? (
+          <Button
+            className={classes.button}
+            size="large"
+            variant="contained"
+            onClick={onEdit}
+          >
+            Редактировать
+          </Button>
+        ) : (
+          <>
+            <Button
+              className={classes.button}
+              type="submit"
+              size="large"
+              variant="contained"
+              onClick={onSave}
+            >
+              Сохранить
+            </Button>
+            <Button
+              className={classes.button}
+              variant="outlined"
+              onClick={onCancelEdit}
+            >
+              Удалить
+            </Button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
